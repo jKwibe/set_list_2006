50460070a972d097438f4b4639ce004f49534587
Brian Zanti <brianzanti@gmail.com>
2020-07-02 15:39:10 -0600

    nested resources

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,5 +5,25 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
+    # songs = Song.all
+    # @song = songs.find do |song|
+    #   song.title == params[:title]
+    # end
+  end
+
+  def new
+    @artist_id = params[:artist_id]
+  end
+
+  def create
+
+    artist = Artist.find(params[:artist_id])
+    new_song = artist.songs.create(song_params)
+    redirect_to("/songs/#{new_song.id}")
+  end
+
+  private
+  def song_params
+    params.permit(:title, :length, :play_count)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
-  get '/songs/:id', to: 'songs#show'
+  get '/songs/:id', to: 'songs#show' #matches get '/songs/1', get '/songs/new'
   get '/artists', to: 'artists#index'
   get '/artists/new', to: 'artists#new'
   post '/artists', to: 'artists#create'
   delete '/artists/:id', to: 'artists#destroy'
   get '/artists/:id/edit', to: 'artists#edit'
   patch '/artists/:id', to: 'artists#update'
+  get '/artists/:artist_id/songs/new', to: 'songs#new'
+  post '/artists/:artist_id/songs', to: 'songs#create'
 end
